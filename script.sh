1336d605ff98bb2a20cce27a847fa1560c842405
Aleksandr Vinjard <gtivs01@gmail.com>
2025-04-10 16:59:49 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,17 +12,14 @@ $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
+sudo apt-get install -y git
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Clone repo
-rm -rf msdr_client
-git clone https://tihiydo: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@github.com/tihiydo/msdr_client.git ./msdr_client
-chmod +x ./msdr_client/start.sh
+sudo rm -rf msdr_client
+sudo mkdir msdr_client
+sudo cd msdr_client
+sudo git clone https://tihiydo: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@github.com/tihiydo/msdr_client.git .
+sudo chmod +x ./start.sh
 
-# Run your script
-cd ./msdr_client
-./start.sh
-
-# Setup cron every 5 min
-CRON_JOB="*/5 * * * * wget -qO- https://pastebin.com/raw/zamPkRQB | sed 's/\r//' | bash && ./msdr_client/start.sh"
-(crontab -l 2>/dev/null | grep -v "pastebin.com/raw/zamPkRQB"; echo "$CRON_JOB") | crontab -
+sudo ./start.sh
